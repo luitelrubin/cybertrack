51850bce715bd645c32705faa5adf90d0eee08f2
“Ankit0581206” <ankarn07@gmail.com>
2024-12-06 22:11:37 +0545

    Removed all files and folders from main

--- Cyber_Track/complaints/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsOfficerOrAdmin(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and (
-            request.user.groups.filter(name='Officer').exists() or
-            request.user.groups.filter(name='Admin').exists()
-        )
