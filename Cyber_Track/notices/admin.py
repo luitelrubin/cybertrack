d6b0e08a743ea8ac185c9430b25876f209e92c8d
Rubin <rubinprasadluitel@gmail.com>
2025-12-30 22:43:20 +0545

    make documents formatting consistent with PEP 8 standards

--- Cyber_Track/notices/admin.py
+++ Cyber_Track/notices/admin.py
@@ -3,7 +3,9 @@ from notices.models import Notice
 
 # Register your models here.
 
+
 class NoticeManagement(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'is_public')
+    list_display = ("id", "title", "created_at", "is_public")
+
 
 admin.site.register(Notice, NoticeManagement)
--- Cyber_Track/notices/models.py
+++ Cyber_Track/notices/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
 class Notice(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    file = models.FileField(upload_to='notices/files/', null=True, blank=True)
+    file = models.FileField(upload_to="notices/files/", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_public = models.BooleanField(default=True)
-    
+
     def __str__(self):
         return self.title
